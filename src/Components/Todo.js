87baa69affc8aea029a47949476446715447edbd
Md. Hasibul Kabir <md_hasibulkabir@mailbox.tu-dresden.de>
2022-04-12 10:27:45 +0200

    feat: add confirmation modal

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import Modal from './Modal'
+import Backdrop from './Backdrop'
+
 function Todo(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function deleteHandler() {
+    setIsModalOpen(true);
+  }
+  function closeModalHandler() {
+    setIsModalOpen(false);
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      { isModalOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> }
+      { isModalOpen && <Backdrop onCancel={closeModalHandler}/> }
     </div>
   );
 }
